eaf118baa73bb03fed3c311167aa81b6ebec28b5
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-04-18 17:59:07 +0530

    removed test console.log

--- app/components/quiz.js
+++ app/components/quiz.js
@@ -2,11 +2,6 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 
 export default class QuizComponent extends Component {
-  constructor() {
-    super(...arguments);
-    console.log(this.args.model);
-  }
-
   @tracked qnCount = 0;
   @tracked demo = this.args.model;
 }
